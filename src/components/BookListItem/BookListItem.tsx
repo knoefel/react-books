15d0e11804a9216db1e504eeeb69fd4138ab4860
Andreas Knöfel <andreas.knoefel@e2m.energy>
2020-01-15 07:11:00 +0100

    feat: delete books - add padding for list item text to fix small viewport width ui issue

--- src/components/BookListItem/BookListItem.tsx
+++ src/components/BookListItem/BookListItem.tsx
@@ -1,8 +1,22 @@
-import { Avatar, IconButton, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText } from "@material-ui/core";
+import {
+  Avatar,
+  IconButton,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  makeStyles
+} from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import React, { FC } from "react";
 import { BookListItemProps } from "./model";
+
+const useStyles = makeStyles({
+  listItemText: {
+    paddingRight: "2.5rem"
+  }
+});
 
 const BookListItem: FC<BookListItemProps> = ({
   book,
@@ -13,12 +27,18 @@ const BookListItem: FC<BookListItemProps> = ({
   const handleClickEdit = () => onClickEdit(book);
   const handleClickDelete = () => onClickDelete(book);
 
+  const classes = useStyles();
+
   return (
     <ListItem classes={{ container: className }} button>
       <ListItemAvatar>
         <Avatar>{book.totalAmount}</Avatar>
       </ListItemAvatar>
-      <ListItemText primary={book.title} secondary={book.author} />
+      <ListItemText
+        className={classes.listItemText}
+        primary={book.title}
+        secondary={book.author}
+      />
       <ListItemSecondaryAction>
         <IconButton onClick={handleClickEdit} edge="end" aria-label="edit">
           <EditIcon />
